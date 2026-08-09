3cc78ef324d9b0b474440853c51b746847584a05
ppG64tre-Cool <pptaratorn@gmail.com>
2026-08-09 12:16:54 +0700

    Update UserinputserviceCam.lua

--- UserinputserviceCam.lua
+++ UserinputserviceCam.lua
@@ -62,7 +62,7 @@ local function setupGyroCamera(character)
 
 			local offset = CFrame.new(0, 0, 0);
 
-			camera.CFrame = CFrame.new((targetPosition * CFrame.new(0, 0, -0.725)).Position) * currentRotation * offset;
+			camera.CFrame = CFrame.new((targetPosition * CFrame.new(0, 0, -0.75)).Position) * currentRotation * offset;
 
 		end);
 
